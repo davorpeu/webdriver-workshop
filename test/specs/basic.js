390865a742725b8bb880a511370f7c6574c596b7
dadop <davorpeu@gmail.com>
2019-11-28 19:04:45 +0100

    neš mal

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,7 +1,21 @@
 const assert = require('assert');
 
-describe('webdriver.io page', () => {
-    it('should have the right title', () => {
-        browser.url('https://webdriver.io');
-    });
+describe("new webmail page", function() {
+    
+    it("Test case title", function() {
+        browser.url("https://webmail.ffos.hr/");
+        $('#rcmloginuser').waitForDisplayed();
+        
+    $('#rcmloginuser').setValue("dpeuraca");
+    $("#rcmloginpwd").setValue("N7prtxMe");
+    $("#rcmloginsubmit").click();
+    browser.pause(5000);
+    $("#rcmbtn110").waitForDisplayed();
+
+       });
+
+       
+    
+
+      
 });
